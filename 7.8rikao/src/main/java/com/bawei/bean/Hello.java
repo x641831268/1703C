d6140fd55ef6ff2895d078f988af2f2ed6f11e05
向 <xiang@DESKTOP-DLGD8TD>
2019-07-10 14:42:45 +0800

    hhguyhjkm

--- 7.8rikao/src/main/java/com/bawei/bean/Hello.java
+++ 7.8rikao/src/main/java/com/bawei/bean/Hello.java
@@ -3,6 +3,6 @@ package com.bawei.bean;
 public class Hello {
      public static void main(String[] args) {
 		System.out.println("aaaa");
-		System.out.println("bbbb");
+		System.out.println("1703");
 	}
 }
